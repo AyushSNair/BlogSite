9f72c0404f5d1c5a60549b72c60e86fedc032587
Ayush Nair <64899298+AyushSNair@users.noreply.github.com>
2025-01-09 23:08:56 +0530

    Update blog.js

--- data/blog.js
+++ data/blog.js
@@ -1,1 +1,19 @@
+blogs = [
+    {
+        Title: "How to get started with React",
+        content: "This is a blog post about getting started with React.",
+        slug: "react-learn"
+    },
+    {
+        Title: "How to get started with Python",
+        content: "This is a blog post about getting started with Python.",
+        slug: "python-learn"
+    },
+    {
+        Title: "How to get started with GenAi",
+        content: "This is a blog post about getting started with GenAi.",
+        slug: "genai-learn"
+    }
+];
 
+module.exports = blogs;
